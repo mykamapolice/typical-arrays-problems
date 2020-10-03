f91b514fdbae1c0430baa8b01d9991a47b1eb899
Ilya Barachenia <ilya.barachenia@gmail.com>
2020-10-03 13:39:01 +0200

    feat: submite typical arrays problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 
 exports.min = function min (array) {
+  if (array !== undefined) {
+    if (array.length !== 0 && array.constructor === Array) {
+      var m = array[0]
+      for (var i = 1; i <= (array.length - 1); i++) {
+        if (m > array[i]) {
+            m = array[i];
+        }
+      }
+      return m;
+    }
+    return 0;
+  }
   return 0;
 }
 
-exports.max = function max (array) {
+
+exports.max = function min (array) {
+  if (array !== undefined) {
+    if (array.length !== 0 && array.constructor === Array) {
+      var m = array[0]
+      for (var i = 1; i <= (array.length - 1); i++) {
+        if (m < array[i]) {
+            m = array[i];
+        }
+      }
+      return m;
+    }
+    return 0;
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array !== undefined) {
+    if (array.length !== 0 && array.constructor === Array) {
+      var m = 0;
+      for (var i = 0; i < (array.length); i++) {
+        m += array[i]
+      }
+      let ans = (m / (array.length));
+      return ans;
+    }
+    return 0;
+  }
   return 0;
 }
